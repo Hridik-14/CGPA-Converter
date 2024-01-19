1e33f56cf3fba864e8651a65d14dc69606022283
Hridik Krishna <hridikgv14@gmail.com>
2024-01-19 11:11:41 +0530

    styling ch anges

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import PDFUpload from '@/components/PDFUpload.vue';
   <div class="absolute md:top-20 top-5 left-1/2 -translate-x-1/2 md:text-4xl text-lg">
     BITS 2 US GPA Converter
   </div>
-  <div class="flex flex-col md:min-w-[530px] max-w-max mx-auto border border-gray-300 rounded md:p-4 px-4 py-2 shadow custom-right-shadow">
+  <div class="flex flex-col md:min-w-[530px] max-w-max mx-auto border border-gray-300 rounded md:p-4 px-4 py-2 shadow custom-right-shadow mt-10 md:mt-0">
     <PDFUpload />
   </div>  
 </template>
